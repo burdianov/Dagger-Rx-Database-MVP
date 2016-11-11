85254721eba807e6c0651da6e7d7cd60e011123d
Leo Burdianov <leo.burdianov@gmail.com>
2016-11-11 13:12:54 +0400

    Create BaseActivity

---
+++ app/src/main/java/com/testography/dagger_rx_database_mvp/BaseActivity.java
@@ -0,0 +1,48 @@
+package com.testography.dagger_rx_database_mvp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getContentView());
+        onViewReady(savedInstanceState, getIntent());
+    }
+
+    /**
+     * onViewReady shall be used by child activities instead of overriding onCreate
+     *
+     * @param savedInstanceState
+     * @param intent
+     */
+    protected void onViewReady(Bundle savedInstanceState, Intent intent) {
+
+    }
+
+    protected void showDialog(String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(true);
+        }
+        mProgressDialog.setMessage(message);
+        mProgressDialog.show();
+    }
+
+    protected void hideDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    protected abstract int getContentView();
+}
--- app/src/main/java/com/testography/dagger_rx_database_mvp/MainActivity.java
+++ app/src/main/java/com/testography/dagger_rx_database_mvp/MainActivity.java
@@ -1,9 +1,8 @@
 package com.testography.dagger_rx_database_mvp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
