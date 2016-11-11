0f5373168939b3122dc9aadb99061fbadd2c8ae3
Leo Burdianov <leo.burdianov@gmail.com>
2016-11-11 13:41:27 +0400

    Create CakeApplication

---
+++ app/src/main/java/com/testography/dagger_rx_database_mvp/application/CakeApplication.java
@@ -0,0 +1,16 @@
+package com.testography.dagger_rx_database_mvp.application;
+
+import android.app.Application;
+
+public class CakeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
